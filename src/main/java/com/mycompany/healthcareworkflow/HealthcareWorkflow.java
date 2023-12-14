7aad3335f81578531de306f739e2bb4df46497aa
BinodLamichhane31 <Lamibinod31@gmail.com>
2023-12-14 18:39:48 +0545

    Login frontend completed

---
+++ src/main/java/com/mycompany/healthcareworkflow/HealthcareWorkflow.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.healthcareworkflow;
+
+/**
+ *
+ * @author Dell
+ */
+public class HealthcareWorkflow {
+
+    public static void main(String[] args) {
+        loginPage login = new loginPage();
+        login.setVisible(true);
+    }
+}
